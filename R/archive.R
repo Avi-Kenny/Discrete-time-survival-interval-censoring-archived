b3d1a1b95fdfd4502fdc5688a578ba22f7744da0
Avi Kenny <avikenny@uw.edu>
2021-06-15 12:22:39 -0700

    Misc

--- R/archive.R
+++ R/archive.R
@@ -16,6 +16,90 @@ if (FALSE) {
 
 
 
+##############################################.
+##### Old code from perform_imputation() #####
+##############################################.
+
+if (FALSE) {
+  
+  dataset$sero_year <- apply(
+    # dataset$sero_year3 <- apply( # !!!!!
+    X = dataset,
+    MARGIN = 1,
+    end_year = attributes(dataset)$end_year,
+    FUN = function(r, end_year) {
+      
+      for (var in c("sex","died","death_year","last_neg_test","first_pos_test",
+                    "birth_year", "case")) {
+        assign(var, as.numeric(r[[var]]))
+      }
+      
+      sex_mf <- ifelse(sex, "male", "female")
+      end_year <- ifelse(died==1, death_year+1, end_year)
+      
+      # Case 1: no testing data
+      if (case==1) {
+        age_end <- end_year - birth_year
+        
+        # probs <- m_probs[[sex_mf]][[age_end]]
+        # !!!!! TESTING
+        if (died==0) {
+          probs <- m_probs[[sex_mf]][[age_end]]
+        } else {
+          probs <- m_probs2[[sex_mf]][[age_end]]
+        }
+        
+        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
+        if (pos==length(probs)) {
+          return (NA)
+        } else {
+          return (birth_year+pos-1)
+        }
+      }
+      
+      # Case 2: most recent test was negative
+      # Iteratively sample using discrete hazards
+      if (case==2) {
+        age_start <- last_neg_test - birth_year + 2
+        age_end <- end_year - birth_year
+        sero_year <- NA
+        age <- age_start
+        while (is.na(sero_year) && age<=age_end) {
+          mult <- ifelse(died,hr_hiv_est,1) # !!!!!
+          if (runif(1)<(p_sero_year[[sex_mf]][age])*mult) { # !!!!!
+            sero_year <- birth_year + age - 1
+          }
+          age <- age + 1
+        }
+        return(sero_year)
+      }
+      
+      # Case 3: negative test followed by a positive test
+      # Sample from a multinomial with probs proportional to discrete hazards
+      if (case==3) {
+        age_start <- last_neg_test - birth_year + 2
+        age_end <- first_pos_test - birth_year + 1
+        probs <- p_sero_year[[sex_mf]][c(age_start:age_end)]
+        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
+        return(last_neg_test + pos)
+      }
+      
+      # Case 4: first test was positive
+      # Sample from a multinomial with probs proportional to discrete hazards
+      if (case==4) {
+        age_start <- 1
+        age_end <- first_pos_test - birth_year + 1
+        probs <- p_sero_year[[sex_mf]][c(age_start:age_end)]
+        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
+        return(birth_year + pos - 1)
+      }
+      
+    }
+  )
+  
+}
+
+
 #####################.
 ##### Version 3 #####
 #####################.
--- R/perform_imputation.R
+++ R/perform_imputation.R
@@ -17,23 +17,33 @@ perform_imputation <- function(dat_baseline, dat_events, theta_m) {
   # dat_events <- dat_events[1:3]
   
   # Perform imputation for each patient
-  x_imputed <- lapply(dat_events, function(d) {
+  x_imputed <- lapply(dat_events, function(de) {
     
     # S_iX is the set of X values with positive posterior probability
     # The actual value assigned represents the number of zeros in X
     S_iX <- case_when(
-      d$case == 1 ~ c(0:d$T_i),
-      d$case == 2 ~ c(d$last_neg_test:d$T_i),
-      d$case == 3 ~ c(d$last_neg_test:(d$first_pos_test-1)),
-      d$case == 4 ~ c(0:(d$first_pos_test-1))
+      de$case == 1 ~ c(0:de$T_i),
+      de$case == 2 ~ c(de$last_neg_test:de$T_i),
+      de$case == 3 ~ c(de$last_neg_test:(de$first_pos_test-1)),
+      de$case == 4 ~ c(0:(de$first_pos_test-1))
     )
     
-    # Calculate component seroconversion discrete hazards
+    # Calculate component discrete hazards
+    # Note: p and db are accessed globally
     p_it <- memoise(function(t) {
       expit(
         p$alpha0 + p$alpha1*db$sex + p$alpha2*(db$b_age+(t-1)/12) +
           p$alpha3*db$u
       )
+    })
+    q_it <- memoise(function(t,x) {
+      min(0.99999, expit(
+        p$gamma0 + p$gamma1*db$sex + p$gamma2*(db$b_age+(t-1)/12) +
+          p$gamma3*db$u
+      ) * exp(
+        log(p$psi1)*x*(1-de$z[t]) +
+          log(p$psi2)*x*de$z[t]
+      ))
     })
     
     # In this block, we assign a probability to each possible value of S_iX
@@ -42,19 +52,19 @@ perform_imputation <- function(dat_baseline, dat_events, theta_m) {
     #     generate_data_events.R and fit_stan.R
     probs <- sapply(S_iX, function(x) {
       
+      # !!!!! Need to QA this
+      
       if (case<=2) {
         
-        # !!!!! CONTINUE
-        
-        if (x==d$last_neg_test) {
+        if (x==de$last_neg_test) {
           P_X <- p_it(x+1)
-        } else if (x %in% c((d$last_neg_test+1):(d$T_i-1))) {
-          P_X_part <- prod(sapply(c((d$last_neg_test+1):x), function(s) {
+        } else if (x %in% c((de$last_neg_test+1):(de$T_i-1))) {
+          P_X_part <- prod(sapply(c((de$last_neg_test+1):x), function(s) {
             (1 - p_it(s))
           }))
           P_X <- P_X_part * p_it(x+1)
-        } else if (x==d$T_i) {
-          P_X_part <- prod(sapply(c((d$last_neg_test+1):d$T_i), function(s) {
+        } else if (x==de$T_i) {
+          P_X_part <- prod(sapply(c((de$last_neg_test+1):de$T_i), function(s) {
             (1 - p_it(s))
           }))
         } else {
@@ -63,11 +73,17 @@ perform_imputation <- function(dat_baseline, dat_events, theta_m) {
         
       } else if (case>=3) {
         
-        P_X <- 999
+        sum_p <- sum(sapply(c((de$last_neg_test+1):de$first_pos_test), p_it))
+        P_X <- p_it(x+1) / sum_p
         
       }
       
-      P_Y <- 999
+      x_vec <- c(rep(0,S_iX),rep(1,de$T_i-S_iX))
+      P_Y_part <- prod(sapply(c(1:(de$T_i-1)), function(s) {
+        1 - q_it(s,x_vec[s])
+      }))
+      q_T_i <- q_it(de$T_i,x_vec[de$T_i])
+      P_Y <- P_Y_part * ifelse(de$y[de$T_i]==1, q_T_i, 1-q_T_i)
       
       return(P_X*P_Y)
       
@@ -78,9 +94,8 @@ perform_imputation <- function(dat_baseline, dat_events, theta_m) {
     mult <- rmultinom(n=1, size=1, prob=probs)
     x_i_sample <- S_iX[mult]
     
-    # Break off NAs (9s)
-    
-    
+    return (c(x_i_sample, rep(9, sum(de$y==9))))
+
   })
   
   # Merge imputations back into dat_events
@@ -90,90 +105,5 @@ perform_imputation <- function(dat_baseline, dat_events, theta_m) {
   }
   
   return(dat_imputed)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # !!!!! Recycle all code below
-  
-  dataset$sero_year <- apply(
-  # dataset$sero_year3 <- apply( # !!!!!
-    X = dataset,
-    MARGIN = 1,
-    end_year = attributes(dataset)$end_year,
-    FUN = function(r, end_year) {
-      
-      for (var in c("sex","died","death_year","last_neg_test","first_pos_test",
-                    "birth_year", "case")) {
-        assign(var, as.numeric(r[[var]]))
-      }
-      
-      sex_mf <- ifelse(sex, "male", "female")
-      end_year <- ifelse(died==1, death_year+1, end_year)
-      
-      # Case 1: no testing data
-      if (case==1) {
-        age_end <- end_year - birth_year
-        
-        # probs <- m_probs[[sex_mf]][[age_end]]
-        # !!!!! TESTING
-        if (died==0) {
-          probs <- m_probs[[sex_mf]][[age_end]]
-        } else {
-          probs <- m_probs2[[sex_mf]][[age_end]]
-        }
-        
-        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
-        if (pos==length(probs)) {
-          return (NA)
-        } else {
-          return (birth_year+pos-1)
-        }
-      }
-      
-      # Case 2: most recent test was negative
-      # Iteratively sample using discrete hazards
-      if (case==2) {
-        age_start <- last_neg_test - birth_year + 2
-        age_end <- end_year - birth_year
-        sero_year <- NA
-        age <- age_start
-        while (is.na(sero_year) && age<=age_end) {
-          mult <- ifelse(died,hr_hiv_est,1) # !!!!!
-          if (runif(1)<(p_sero_year[[sex_mf]][age])*mult) { # !!!!!
-            sero_year <- birth_year + age - 1
-          }
-          age <- age + 1
-        }
-        return(sero_year)
-      }
-      
-      # Case 3: negative test followed by a positive test
-      # Sample from a multinomial with probs proportional to discrete hazards
-      if (case==3) {
-        age_start <- last_neg_test - birth_year + 2
-        age_end <- first_pos_test - birth_year + 1
-        probs <- p_sero_year[[sex_mf]][c(age_start:age_end)]
-        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
-        return(last_neg_test + pos)
-      }
-      
-      # Case 4: first test was positive
-      # Sample from a multinomial with probs proportional to discrete hazards
-      if (case==4) {
-        age_start <- 1
-        age_end <- first_pos_test - birth_year + 1
-        probs <- p_sero_year[[sex_mf]][c(age_start:age_end)]
-        pos <- which(as.numeric(rmultinom(n=1, size=1, prob=probs))==1)
-        return(birth_year + pos - 1)
-      }
-      
-    }
-  )
   
 }
